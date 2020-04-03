9cbc8211d0a0d8129b979e218d88fa98661ba762
mahaliroblesarbieto <mahaliroblesarbieto@gmail.com>
2020-04-03 09:11:39 -0500

    primeras configuraciones de proyecto

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <Header titulo="Cotizador de Seguro Vehicular"/>
+  );
+}
+
+export default App;
